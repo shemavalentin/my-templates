cbc21ee2f6d2a63ceb418c3b4b7205a459940f8a
Shema Valentin <shemavalentin@gmail.com>
2024-02-01 18:35:10 +0200

    commit about reviewed codes

---
+++ script.js
@@ -0,0 +1,81 @@
+
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Getting Quotes from API
+
+let apiQuotes = [];
+
+function showLoadingSpinner() {
+    loader.hidden = false;
+    quoteContainer.hidden = true
+}
+
+// Hide loading
+
+function removeLoadingnSpinner() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+//Show new Quotes
+
+function newQuote() { 
+    showLoadingSpinner();
+    // pick a random quote from apiQuote array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+
+    //Check if the author field is blank and replace it with 'Unknown'
+    if (!quote.author) {
+        authorText.textContent = 'Unknown';
+    } else {
+        authorText.textContent = quote.author;
+    }
+
+    //Check Quote length to determine styling
+
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }   
+    quoteText.textContent = quote.text;
+    showLoadingSpinner();
+}
+
+// Get Quotes From API
+ 
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
+
+    try {
+        const response = await fetch(apiUrl);
+        apiQuotes = await response.json();
+        newQuote();
+
+    } catch (error) {
+        // catch errors
+   
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+
+    window.open(twitterUrl, '_blank');
+}
+//event listenner
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+// on load
+
+getQuotes()
+
+// loading();
